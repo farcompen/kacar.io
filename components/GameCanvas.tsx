69c251109430be59fe2b5cd6b5c0eb8624de6f98
farcompen <farcompen71@gmail.com>
2025-11-21 10:30:37 +0300

    kütle 100000 olunca daha küçük bot olusturulacak

--- components/GameCanvas.tsx
+++ components/GameCanvas.tsx
@@ -493,7 +493,7 @@ const GameCanvas: React.FC<GameCanvasProps> = ({ nickname, setGameState, setScor
          }
          }
          else {
-          spawnRadius= Math.floor(Math.random() * 30) + 10;
+          spawnRadius = Math.max(20, avgPlayerRadius * 0.08);
          }
          
 
